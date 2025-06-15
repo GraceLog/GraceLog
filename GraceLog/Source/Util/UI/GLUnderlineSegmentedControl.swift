c40b22c91ed3796d521b85134855dc62ec17c29b
dlrjswns <leekyunjun@gmail.com>
2025-06-15 18:10:05 +0900

    [ADD] GLUnderlineSegmentedControl 파일 생성

---
+++ GraceLog/Source/Util/UI/GLUnderlineSegmentedControl.swift
@@ -0,0 +1,12 @@
+//
+//  GLUnderlineSegmentedControl.swift
+//  GraceLog
+//
+//  Created by 이건준 on 6/15/25.
+//
+
+import Foundation
+
+final class GLUnderlineSegmentedControl: UISegmentedControl {
+  
+}
