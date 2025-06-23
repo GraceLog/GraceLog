15e136d26b2f2c380e9e78ef8441f8bca66727a8
SangJLee1103 <sangjlee1103@gmail.com>
2025-06-23 22:02:57 +0900

    [FEAT] UIPageViewController를 통한 홈 ViewController 분리(개인, 공동체)

--- GraceLog/Source/Presentation/HomeScene/ViewController/HomeViewController.swift
+++ GraceLog/Source/Presentation/HomeScene/ViewController/HomeViewController.swift
@@ -24,7 +24,7 @@ enum HomeMenuItem: CaseIterable {
     }
 }
 
-final class HomeViewController: GraceLogBaseViewController, View {
+final class HomeViewController: UIPageViewController, View {
     weak var coordinator: Coordinator?
     var disposeBag = DisposeBag()
     
@@ -53,66 +53,24 @@ final class HomeViewController: GraceLogBaseViewController, View {
         $0.setDimensions(width: 32, height: 32)
     }
     
-    private lazy var tableView = UITableView(frame: .zero, style: .grouped).then {
-        $0.backgroundColor = UIColor(hex: 0xF4F4F4)
-        $0.separatorStyle = .none
-        $0.sectionHeaderTopPadding = 0
-        $0.sectionFooterHeight = 0
-        $0.estimatedSectionHeaderHeight = 100
-        $0.sectionHeaderHeight = UITableView.automaticDimension
+    private lazy var homeMyViewController = HomeMyViewController()
+    private lazy var homeCommunityViewController = HomeCommunityViewController()
+    
+    private lazy var pages: [UIViewController] = [
+        homeMyViewController,
+        homeCommunityViewController
+    ]
+    
+    init() {
+        super.init(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal
+        )
     }
     
-    private lazy var dataSource = RxTableViewSectionedReloadDataSource<HomeSectionModel>(
-        configureCell: { [weak self] dataSource, tableView, indexPath, item in
-            switch dataSource[indexPath.section] {
-            case .diary:
-                let cell = tableView.dequeueReusableCell(withIdentifier: HomeDiaryTableViewCell.identifier, for: indexPath) as! HomeDiaryTableViewCell
-                cell.selectionStyle = .none
-                
-                if let diaryItem = item as? [MyDiaryItem] {
-                    cell.configure(with: diaryItem)
-                }
-                
-                if let username = self?.reactor?.currentState.user?.name {
-                    cell.setTitle(username: username)
-                }
-                
-                return cell
-            case .contentList:
-                let cell = tableView.dequeueReusableCell(withIdentifier: HomeRecommendTableViewCell.identifier, for: indexPath) as! HomeRecommendTableViewCell
-                cell.selectionStyle = .none
-                
-                if let videoItem = item as? HomeVideoItem {
-                    let image = UIImage(named: videoItem.imageName) ?? UIImage()
-                    cell.configure(title: videoItem.title, image: image)
-                }
-                return cell
-            case .communityButtons:
-                let cell = tableView.dequeueReusableCell(withIdentifier: CommunityTableViewCell.identifier, for: indexPath) as! CommunityTableViewCell
-                cell.selectionStyle = .none
-                
-                if let communityItem = item as? [CommunityItem] {
-                    cell.configure(with: communityItem)
-                    return cell
-                }
-                return cell
-            case .communityPosts:
-                let diaryItem = item as! CommunityDiaryItem
-                switch diaryItem.type {
-                case .my:
-                    let cell = tableView.dequeueReusableCell(withIdentifier: HomeCommunityMyTableViewCell.identifier, for: indexPath) as! HomeCommunityMyTableViewCell
-                    cell.selectionStyle = .none
-                    cell.configure(title: diaryItem.title, subtitle: diaryItem.subtitle, likes: diaryItem.likes, comments: diaryItem.comments)
-                    return cell
-                case .regular:
-                    let cell = tableView.dequeueReusableCell(withIdentifier: HomeCommunityUserTableViewCell.identifier, for: indexPath) as! HomeCommunityUserTableViewCell
-                    cell.selectionStyle = .none
-                    cell.configure(username: diaryItem.username, title: diaryItem.title, subtitle: diaryItem.subtitle, likes: diaryItem.likes, comments: diaryItem.comments)
-                    return cell
-                }
-            }
-        }
-    )
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -127,9 +85,17 @@ final class HomeViewController: GraceLogBaseViewController, View {
             ),
             homeRepository: DefaultHomeRepository()
         ))
+        
+        configurePageViewController()
         configureUI()
-        configureTableView()
         configureNavBar()
+    }
+    
+    private func configurePageViewController() {
+        dataSource = self
+        delegate = self
+        
+        setViewControllers([pages[0]], direction: .forward, animated: false, completion: nil)
     }
     
     private func configureUI() {
@@ -140,12 +106,6 @@ final class HomeViewController: GraceLogBaseViewController, View {
             $0.top.leading.trailing.equalTo(safeArea)
             $0.height.equalTo(50)
         }
-        
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints {
-            $0.top.equalTo(navigationBar.snp.bottom)
-            $0.leading.trailing.bottom.equalTo(safeArea)
-        }
     }
     
     private func configureNavBar() {
@@ -154,87 +114,19 @@ final class HomeViewController: GraceLogBaseViewController, View {
         navigationBar.addRightItem(profileButton)
     }
     
-    private func configureTableView() {
-        // User
-        tableView.register(HomeTableViewHeader.self, forHeaderFooterViewReuseIdentifier: HomeTableViewHeader.identifier)
-        tableView.register(HomeDiaryTableViewCell.self, forCellReuseIdentifier: HomeDiaryTableViewCell.identifier)
-        tableView.register(HomeRecommendTableViewCell.self, forCellReuseIdentifier: HomeRecommendTableViewCell.identifier)
-        tableView.register(CommunityTableViewCell.self, forCellReuseIdentifier: CommunityTableViewCell.identifier)
-        
-        // Community
-        tableView.register(CommunityTableViewCell.self, forCellReuseIdentifier: CommunityTableViewCell.identifier)
-        tableView.register(HomeCommunityDateHeaderView.self, forHeaderFooterViewReuseIdentifier: HomeCommunityDateHeaderView.identifier)
-        tableView.register(HomeCommunityUserTableViewCell.self, forCellReuseIdentifier: HomeCommunityUserTableViewCell.identifier)
-        tableView.register(HomeCommunityMyTableViewCell.self, forCellReuseIdentifier: HomeCommunityMyTableViewCell.identifier)
-    }
-    
-    // MARK: - 유저 업데이트 시 NOTI
-    override func onUserProfileUpdated(_ user: GraceLogUser) {
-        reactor?.action.onNext(.updateUser(user))
-    }
-    
     func bind(reactor: HomeViewReactor) {
         // Action
         homeMenuView.rx.value
             .map { HomeMenuItem.allCases[$0] }
             .bind(with: self) { owner, selectedMenu in
+                let targetIndex = selectedMenu == .user ? 0 : 1
+                owner.moveToPage(at: targetIndex, animated: true)
+                
                 switch selectedMenu {
                 case .user:
                     reactor.action.onNext(HomeViewReactor.Action.userButtonTapped)
                 case .group:
                     reactor.action.onNext(HomeViewReactor.Action.groupButtonTapped)
-                }
-            }
-            .disposed(by: disposeBag)
-        
-        tableView.rx.setDelegate(self)
-            .disposed(by: disposeBag)
-        
-        tableView.rx.modelSelected(Any.self)
-            .subscribe(onNext: { item in
-                if let communityItem = item as? CommunityItem {
-                    reactor.action.onNext(.selectCommunity(item: communityItem))
-                }
-            })
-            .disposed(by: disposeBag)
-        
-        tableView.rx.willDisplayCell
-            .subscribe(onNext: { [weak self] cell, indexPath in
-                guard let self = self, let reactor = self.reactor else { return }
-                
-                if let communityCell = cell as? CommunityTableViewCell {
-                    communityCell.communityButtonTapped
-                        .take(1)
-                        .map { HomeViewReactor.Action.selectCommunity(item: $0) }
-                        .bind(to: reactor.action)
-                        .disposed(by: self.disposeBag)
-                }
-            })
-            .disposed(by: disposeBag)
-        
-        // State
-        reactor.state
-            .map { $0.sections }
-            .bind(to: tableView.rx.items(dataSource: dataSource))
-            .disposed(by: disposeBag)
-        
-        reactor.state
-            .map { $0.currentSegment }
-            .distinctUntilChanged()
-            .asDriver(onErrorDriveWith: .empty())
-            .drive(with: self) { owner, selectedSegment in
-                let selectedSegmentIndex = selectedSegment == .user ? 0 : 1
-                owner.homeMenuView.selectedSegmentIndex = selectedSegmentIndex
-                owner.tableView.reloadData()
-            }
-            .disposed(by: disposeBag)
-        
-        reactor.state
-            .map { $0.selectedCommunity }
-            .distinctUntilChanged()
-            .subscribe(with: self) { owner, _ in
-                DispatchQueue.main.async {
-                    owner.tableView.reloadData()
                 }
             }
             .disposed(by: disposeBag)
@@ -256,59 +148,53 @@ final class HomeViewController: GraceLogBaseViewController, View {
             }
             .disposed(by: disposeBag)
     }
+    
+    private func moveToPage(at index: Int, animated: Bool) {
+        guard index < pages.count,
+              let currentViewController = viewControllers?.first,
+              let currentIndex = pages.firstIndex(of: currentViewController),
+              currentIndex != index else { return }
+        
+        let direction: UIPageViewController.NavigationDirection = index > currentIndex ? .forward : .reverse
+        
+        setViewControllers(
+            [pages[index]],
+            direction: direction,
+            animated: animated,
+            completion: nil
+        )
+    }
 }
 
-extension HomeViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard let reactor = reactor else { return nil }
+// MARK: - UIPageViewControllerDataSource, UIPageViewControllerDelegate
+extension HomeViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
         
-        switch reactor.currentState.currentSegment {
-        case .user:
-            guard let homeSection = HomeSection(rawValue: section) else { return UIView() }
-            
-            switch homeSection {
-            case .diary:
-                let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: HomeTableViewHeader.identifier) as! HomeTableViewHeader
-                header.configure(
-                    title: "오늘의 말씀",
-                    desc: "순종이 제사보다 낫고 듣는 것이 숫양의 기름보다 나으니",
-                    paragraph: "사무엘상 5:22"
-                )
-                return header
-            case .contentList:
-                let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: HomeTableViewHeader.identifier) as! HomeTableViewHeader
-                header.configure(title: "추천영상", desc: "#순종 #도전", paragraph: nil)
-                return header
-            }
-            
-        case .group:
-            if section == 0 {
-                return nil
-            } else {
-                let sectionModel = reactor.currentState.sections[section]
-                
-                if case let .communityPosts(date, _) = sectionModel {
-                    let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: HomeCommunityDateHeaderView.identifier) as! HomeCommunityDateHeaderView
-                    header.configure(date: date)
-                    return header
-                }
-                
-                return nil
-            }
-        }
+        guard currentIndex > 0 else { return nil }
+        return pages[currentIndex - 1]
     }
     
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        guard let reactor = reactor else { return 0 }
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
         
-        switch reactor.currentState.currentSegment {
-        case .user:
-            return UITableView.automaticDimension
-        case .group:
-            if section == 0 {
-                return .leastNonzeroMagnitude
-            } else {
-                return UITableView.automaticDimension
+        guard currentIndex < (pages.count - 1) else { return nil}
+        return pages[currentIndex + 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        guard completed,
+              let currentViewController = pageViewController.viewControllers?.first,
+              let index = pages.firstIndex(of: currentViewController) else { return }
+        
+        homeMenuView.selectedSegmentIndex = index
+        let selectedSegment: HomeViewReactor.State.HomeModeSegment = index == 0 ? .user : .group
+        if selectedSegment != reactor?.currentState.currentSegment {
+            switch selectedSegment {
+            case .user:
+                reactor?.action.onNext(.userButtonTapped)
+            case .group:
+                reactor?.action.onNext(.groupButtonTapped)
             }
         }
     }
