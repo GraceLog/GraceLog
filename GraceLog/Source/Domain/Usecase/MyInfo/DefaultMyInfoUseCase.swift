12ed6a69987a684599e20948892d5f3f69d647f5
SangJLee1103 <sangjlee1103@gmail.com>
2025-08-10 15:50:07 +0900

    [REFACTOR] ProfileEdit 리팩토링

--- GraceLog/Source/Domain/Usecase/MyInfo/DefaultMyInfoUseCase.swift
+++ GraceLog/Source/Domain/Usecase/MyInfo/DefaultMyInfoUseCase.swift
@@ -11,6 +11,7 @@ import RxRelay
 
 final class DefaultMyInfoUseCase: MyInfoUseCase {
     var updateUserResult = PublishRelay<Bool>()
+    var profileImageData = PublishRelay<Data?>()
     
     private let userRepository: UserRepository
     private let disposeBag = DisposeBag()
@@ -19,12 +20,30 @@ final class DefaultMyInfoUseCase: MyInfoUseCase {
         self.userRepository = userRepository
     }
     
+    func loadProfileImageData() {
+        guard let profileImageURL = UserManager.shared.profileImageURL else {
+            profileImageData.accept(nil)
+            return
+        }
+        
+        GLImageUtil.shared.loadImageData(from: profileImageURL)
+            .subscribe(
+                onSuccess: { [weak self] data in
+                    self?.profileImageData.accept(data)
+                },
+                onFailure: { [weak self] _ in
+                    self?.profileImageData.accept(nil)
+                }
+            )
+            .disposed(by: disposeBag)
+    }
+    
     func updateUser(
         name: String,
         nickname: String,
         profileImage: Data?,
         message: String
-    ) { 
+    ) {
         userRepository.updateUser(
             name: name,
             nickname: nickname,
