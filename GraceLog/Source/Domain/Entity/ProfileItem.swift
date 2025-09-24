a49bbfab9fadceddd26af4bb15329d31c84c2088
dlrjswns <leekyunjun@gmail.com>
2025-09-24 23:01:45 +0900

    [FEAT] ProfileItem 모델 추가
    - 공동체 검색관련 프로필관련 모델 정의

---
+++ GraceLog/Source/Domain/Entity/ProfileItem.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileItem.swift
+//  GraceLog
+//
+//  Created by 이건준 on 9/24/25.
+//
+
+import Foundation
+
+struct ProfileItem {
+    let id: Int
+    let name: String
+    let imageURL: URL?
+}
