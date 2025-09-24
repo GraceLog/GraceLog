b0629d5379499283c30233a27eae2c4d81304331
dlrjswns <leekyunjun@gmail.com>
2025-09-24 23:01:11 +0900

    [FEAT] CommunityChatting모델 추가
    - 공동체 채팅방에 관련된 엔티티 모델 정의

---
+++ GraceLog/Source/Domain/Entity/CommunityChatting.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityChatting.swift
+//  GraceLog
+//
+//  Created by 이건준 on 9/21/25.
+//
+
+import Foundation
+
+struct CommunityChatting {
+    let id: Int
+    let title: String
+    let description: String
+    let recentEditedDate: Date?
+    let peopleCount: Int
+    let imageURL: URL?
+}
